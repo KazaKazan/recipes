2993f6879b1cfc550c5cef3dfffa7f8e91161a4d
KazaKazan <ahmetfiratusta@hotmail.com>
2021-11-16 15:35:59 +0300

    Connect page buttons to functions.

--- script.js
+++ script.js
@@ -1,46 +1,34 @@
 const landingCard = document.getElementById("landingCard");
 const contPrompt = document.getElementById("contPrompt");
+const pageZero = document.getElementById("infoButton");
+const pageOne = document.getElementById("ingButton");
+const pageTwo = document.getElementById("recButton");
+
+let currentRecipe = 0;
 
 contPrompt.onclick = () => landingCard.classList.add("hidden");
-
-class Recipe {
-    name;
-    info;
-    ingredients;
-    steps;
-    restrictions;
-    constructor(name,info,ingredients,steps,restrictions){
-        this.name = name;
-        this.info = info;
-        this.ingredients = ingredients;
-        this.steps = steps;
-        this.restrictions = restrictions;
-    }
-}
+pageZero.onclick = () => displayPage(0);
+pageOne.onclick = () => displayPage(1);
+pageTwo.onclick = () => displayPage(2);
 
 var request = new XMLHttpRequest();
 request.open("GET", "recipes.json", false);
 request.send(null)
 var recipeJSON = JSON.parse(request.responseText);
 
-console.log(recipeJSON[0].name)
-
-let currentRecipe = 0;
-let currentPage = 0;
-
-function displayPage(){
-    console.log("Displaying page " + currentPage)
+function displayPage(pageNumber){
+    console.log("Displaying page " + pageNumber)
 }
 
 function displayRecipe(){
     console.log("Displaying recipe " + currentRecipe);
 
-    currentPage = 0;
-
     const recipeImage = document.getElementById("recipeImage");
     recipeImage.setAttribute("src","assets/"+currentRecipe+".jpg");
     const recipeName = document.getElementById("recipeName");
     recipeName.textContent = recipeJSON[currentRecipe].name;
+    
+    displayPage(0)
 }
 
 displayRecipe()
