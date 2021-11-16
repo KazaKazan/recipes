01f809f744038b37277210fd9a114d2976f72b12
KazaKazan <ahmetfiratusta@hotmail.com>
2021-11-16 14:32:05 +0300

    Make recipe image container work.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 const landingCard = document.getElementById("landingCard");
 const contPrompt = document.getElementById("contPrompt");
 
+contPrompt.onclick = () => landingCard.classList.add("hidden");
 
-contPrompt.onclick = () => landingCard.classList.add("hidden")
+let currentRecipe = 0;
+
+function displayRecipe(recipeNumber){
+    console.log("Displaying recipe " + recipeNumber)
+
+    const recipeImage = document.getElementById("recipeImage")
+    recipeImage.setAttribute("src","assets/"+recipeNumber+".jpg")
+}
+
+displayRecipe(currentRecipe)
