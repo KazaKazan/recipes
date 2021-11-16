dbb2f7de0645c57b86559f71ef2e0fad880317d4
KazaKazan <ahmetfiratusta@hotmail.com>
2021-11-16 15:43:02 +0300

    Expand displayPage function.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const contPrompt = document.getElementById("contPrompt");
 const pageZero = document.getElementById("infoButton");
 const pageOne = document.getElementById("ingButton");
 const pageTwo = document.getElementById("recButton");
+const rightContent = document.getElementById("rightContent");
 
 let currentRecipe = 0;
 
@@ -13,22 +14,31 @@ pageTwo.onclick = () => displayPage(2);
 
 var request = new XMLHttpRequest();
 request.open("GET", "recipes.json", false);
-request.send(null)
+request.send(null);
 var recipeJSON = JSON.parse(request.responseText);
 
 function displayPage(pageNumber){
-    console.log("Displaying page " + pageNumber)
-}
+    while (rightContent.firstChild) {
+        rightContent.firstChild.remove()
+    };
+
+    switch(pageNumber){
+        case 0:
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+    };
+};
 
 function displayRecipe(){
-    console.log("Displaying recipe " + currentRecipe);
-
     const recipeImage = document.getElementById("recipeImage");
     recipeImage.setAttribute("src","assets/"+currentRecipe+".jpg");
     const recipeName = document.getElementById("recipeName");
     recipeName.textContent = recipeJSON[currentRecipe].name;
     
     displayPage(0)
-}
+};
 
 displayRecipe()
